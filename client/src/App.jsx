3281c3b376e9731dcf557e698d96ec5a56585980
metydebebe <metydebebe3@gmail.com>
2025-02-11 21:37:46 +0300

    Home page frontend added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Home from './Components/HomePage/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-
+const App = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/home" element={<Home />} />
+        {/* add other routes*/}
+ 
+      </Routes>
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
